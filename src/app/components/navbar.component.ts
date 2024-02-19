d38b1d5ca878705cd643fdf8fedee34f2ea6e9fd
Rohit1024 <rohitkharche1024@gmail.com>
2024-02-19 10:54:13 +0530

    Updated Theme chips

--- src/app/components/navbar.component.ts
+++ src/app/components/navbar.component.ts
@@ -1,12 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemType, ThemeService, allThemes } from '../theme.service';
-import { ThemePreviewComponent } from './preview.component';
+import { ThemeChipComponent } from './theme-preview.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, ThemePreviewComponent],
+  imports: [CommonModule, ThemeChipComponent],
   template: ` <div class="navbar bg-primary">
     <div class="flex-1">
       <a class="btn btn-ghost text-xl text-primary-content">daisyUI</a>
@@ -14,25 +14,23 @@ import { ThemePreviewComponent } from './preview.component';
     <div class="flex-none">
       <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn m-1">Theme</div>
-        <ul
+        <div
           tabindex="0"
-          class="dropdown-content z-[1] overflow-y-auto block h-[400px] menu p-2 shadow bg-base-200 rounded-box w-56"
+          class="flex flex-col dropdown-content gap-2 mt-2 z-10 p-4 shadow-2xl bg-base-200 rounded-box w-64 h-72 overflow-y-scroll"
         >
-          @for (theme of themes(); track theme) {
-          <li>
-            <div class="w-full" (click)="toggleTheme(theme)">
-              <theme-preview [theme]="theme" />
-            </div>
-          </li>
+          @for (theme of themes; track theme) {
+          <a (click)="toggleTheme(theme)">
+            <theme-preview-chip [theme]="theme" />
+          </a>
           }
-        </ul>
+        </div>
       </div>
     </div>
   </div>`,
 })
 export class NavbarComponent {
   public themService = inject(ThemeService);
-  themes = signal<typeof allThemes>(allThemes);
+  themes = allThemes;
   toggleTheme(theme: ThemType) {
     this.themService.setTheme(theme);
   }
